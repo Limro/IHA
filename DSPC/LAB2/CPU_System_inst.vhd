0817c738e7dcd83081efb7cafec5e42475f29d02
Rasmus Bækgaard <git@bakgaard.net>
2013-02-01 15:21:01 +0100

    DSPC LAB2 part1 works

---
+++ DSPC/LAB2/CPU_System_inst.vhd
@@ -0,0 +1,23 @@
+  --Example instantiation for system 'CPU_System'
+  CPU_System_inst : CPU_System
+    port map(
+      LCD_E_from_the_lcd_0 => LCD_E_from_the_lcd_0,
+      LCD_RS_from_the_lcd_0 => LCD_RS_from_the_lcd_0,
+      LCD_RW_from_the_lcd_0 => LCD_RW_from_the_lcd_0,
+      LCD_data_to_and_from_the_lcd_0 => LCD_data_to_and_from_the_lcd_0,
+      SRAM_ADDR_from_the_sram_0 => SRAM_ADDR_from_the_sram_0,
+      SRAM_CE_N_from_the_sram_0 => SRAM_CE_N_from_the_sram_0,
+      SRAM_DQ_to_and_from_the_sram_0 => SRAM_DQ_to_and_from_the_sram_0,
+      SRAM_LB_N_from_the_sram_0 => SRAM_LB_N_from_the_sram_0,
+      SRAM_OE_N_from_the_sram_0 => SRAM_OE_N_from_the_sram_0,
+      SRAM_UB_N_from_the_sram_0 => SRAM_UB_N_from_the_sram_0,
+      SRAM_WE_N_from_the_sram_0 => SRAM_WE_N_from_the_sram_0,
+      clocks_0_sys_clk_out => clocks_0_sys_clk_out,
+      out_port_from_the_pio_output_0 => out_port_from_the_pio_output_0,
+      out_port_from_the_pio_output_1 => out_port_from_the_pio_output_1,
+      clk_0 => clk_0,
+      in_port_to_the_pio_input_0 => in_port_to_the_pio_input_0,
+      reset_n => reset_n
+    );
+
+

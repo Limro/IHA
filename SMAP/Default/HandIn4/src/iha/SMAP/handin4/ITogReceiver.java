90a314ddc2b825f72d0bdebf552b899f3a9938c9
Rasmus Bækgaard <git@bakgaard.net>
2013-05-05 23:27:35 +0200

    Added HandIn4 WIP

---
+++ SMAP/Default/HandIn4/src/iha/SMAP/handin4/ITogReceiver.java
@@ -0,0 +1,19 @@
+package iha.SMAP.handin4;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ITogReceiver extends BroadcastReceiver
+{
+	public ITogReceiver()
+	{}
+
+	@Override
+	public void onReceive(Context context, Intent intent)
+	{
+		// TODO: This method is called when the BroadcastReceiver is receiving
+		// an Intent broadcast.
+		
+	}
+}

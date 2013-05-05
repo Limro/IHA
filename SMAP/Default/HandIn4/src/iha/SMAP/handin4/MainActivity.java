5aef37b6e978dc1dd05397a3135491d943ad7c42
Rasmus Bækgaard <git@bakgaard.net>
2013-05-05 23:52:02 +0200

    Removed unesesary code

--- SMAP/Default/HandIn4/src/iha/SMAP/handin4/MainActivity.java
+++ SMAP/Default/HandIn4/src/iha/SMAP/handin4/MainActivity.java
@@ -1,102 +1,54 @@
 package iha.SMAP.handin4;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
+	private EditText filterText = null;
+	ArrayAdapter<String> adapter = null;
+
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+		filterText.removeTextChangedListener(filterTextWatcher);
+	}
+
 	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		String result = null;
-		URI myURI = null;
-
-		try {
-			myURI = new URI("itog/action/list/format/json");
-		} catch (URISyntaxException e) {
-			// Deal with it
-		}
-		HttpClient httpClient = new DefaultHttpClient();
-		HttpGet getMethod = new HttpGet(myURI);
-		HttpResponse webServerResponse = null;
-		try {
-			webServerResponse = httpClient.execute(getMethod);
-		} catch (ClientProtocolException e) {
-		} catch (IOException e) {
-		}
-
-		HttpEntity httpEntity = webServerResponse.getEntity();
-
-		if (httpEntity != null) {
-			InputStream instream;
-			try {
-				instream = httpEntity.getContent();
-				result = convertStreamToString(instream);
-				instream.close();
-			} catch (IllegalStateException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-		}
-
-		JSONObject myJSON = null;
-
-		try {
-			myJSON = new JSONObject(result);
-			JSONArray array = myJSON.getJSONArray("items");
-			for (int i = 0; i < array.length(); i++) {
-				JSONObject windJSON = array.getJSONObject(i);
-
-				String name = windJSON.getString("name");
-				Log.d("WINDSPOTTER", name);
-
-				}
-		} catch (JSONException e) {
-		}
-
+		FilterListener();
 	}
 
-	public String convertStreamToString(InputStream is) throws IOException {
-		if (is != null) {
-			StringBuilder sb = new StringBuilder();
-			String line = null;
- 
-			try {
-				BufferedReader reader = new BufferedReader(
-						new InputStreamReader(is, "UTF-8"));
-				while ((line = reader.readLine()) != null) {
-					sb.append(line).append("\n");
-				}
-			} finally {
-				is.close();
-			}
-			return sb.toString();
-		} else {
-			return "";
+	private TextWatcher filterTextWatcher = new TextWatcher()
+	{
+		@Override
+		public void afterTextChanged(Editable s)
+		{}
+
+		public void beforeTextChanged(CharSequence s, int start, int count, int after)
+		{}
+
+		public void onTextChanged(CharSequence s, int start, int before, int count)
+		{
+			adapter.getFilter().filter(s);
 		}
+	};
+
+	private void FilterListener()
+	{
+	    filterText = (EditText) findViewById(R.id.editText1);
+	    filterText.addTextChangedListener(filterTextWatcher);
+
+	    //setListAdapter(new ArrayAdapter<String>(this, android.R.layout.list_content, getStringArrayList());
 	}
 }

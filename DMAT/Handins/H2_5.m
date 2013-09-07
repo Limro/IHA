2cd47f2dca835a9f0b888dcaaa678d4c67355a81
Rasmus Bækgaard <git@bakgaard.net>
2013-09-08 01:21:33 +0200

    Added DMAT handin 2

--- DMAT/Handins/H2_5.m
+++ DMAT/Handins/H2_5.m
@@ -1,12 +1,10 @@
-n=0.1:1:10;
+n=-7:1:7;
 f=power(n,3)-5*n-8;
 figure(1)
 subplot(1,1,1)
 plot(n, f, 's', n, f)
-axis([0 5 -25 100])
+axis([-5 5 -25 100])
 title('f(n)')
-set(gca, 'XTick', [0:1:6])
+set(gca, 'XTick', [0:1:7])
 set(gca, 'YTick', [0])
 grid on
-
-

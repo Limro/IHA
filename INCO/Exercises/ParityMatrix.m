3bbd0a4ad73058af66f4a9b05dcb36c368882b27
Rasmus Bækgaard <git@bakgaard.net>
2014-03-10 09:07:10 +0100

    Add new slides to INCO

--- INCO/Exercises/ParityMatrix.m
+++ INCO/Exercises/ParityMatrix.m
@@ -1,10 +1,10 @@
-function [res] = ParityMatrix(G)
+function [res, P] = ParityMatrix(G)
 %Parity check matrix.
 
-[r, c] = size(G)
-nk = c - log2(r)
-P = G(:,1:nk)
-EyeSize = length(P(1,:))
-[res] = [eye(EyeSize) P']
+[r, c] = size(G);
+nk = c - log2(r);
+P = G(:,1:nk);
+EyeSize = length(P(1,:));
+res = [eye(EyeSize) P'];
 
 end

2d984f13c090fc18ff003141dce1fd7c63a622c7
Rasmus Bækgaard <git@bakgaard.net>
2014-04-04 12:38:30 +0200

    Update PLPA script for PowerShell command

--- PLPA/Scripts/pro1.pl
+++ PLPA/Scripts/pro1.pl
@@ -1,6 +1,10 @@
 #!'C:\Program Files (x86)\Yap\bin\yap' -L --
-assert((append([], Xs,Xs))).
-assertz((append([X|Ys],Zs,[X|Ws]):-append(Ys, Zs,Ws))).
 
-assert( rev([],[]) ).
-assertz(( rev([X|Xs], Output) :- rev(Xs,RevXs), append(RevXs, [X], Output) )).
+:- assert((append([], Xs,Xs))).
+:- assertz((append([X|Ys],Zs,[X|Ws]):-append(Ys, Zs,Ws))).
+
+:- assert( rev([],[]) ).
+:- assertz(( rev([X|Xs], Output) :- rev(Xs,RevXs), append(RevXs, [X], Output) )).
+
+:- rev([1,4],X).
+:- write(X),nl.

606136aa00d3529ebad38673b929b8aed81cafa9
Rasmus Bækgaard <git@bakgaard.net>
2014-04-04 11:26:12 +0200

    Add first lecture notes and scripts

---
+++ PLPA/Scripts/hello.pl
@@ -0,0 +1,6 @@
+#!'C:\Program Files (x86)\Yap\bin\yap' -L --
+#
+# Hello World script file using YAP
+# .
+:- initialization(main).
+main :- write('Hello World'), nl.
---
+++ PLPA/Scripts/pro1.pl
@@ -0,0 +1,6 @@
+#!'C:\Program Files (x86)\Yap\bin\yap' -L --
+assert((append([], Xs,Xs))).
+assertz((append([X|Ys],Zs,[X|Ws]):-append(Ys, Zs,Ws))).
+
+assert( rev([],[]) ).
+assertz(( rev([X|Xs], Output) :- rev(Xs,RevXs), append(RevXs, [X], Output) )).

cf969fdf1a3d4bb8b74583d77178e70a62fdfa8e
Rasmus Bækgaard <git@bakgaard.net>
2014-04-28 14:53:40 +0200

    SMP notes and exercises

--- SMP/Exercises/Chapter3.m
+++ SMP/Exercises/Chapter3.m
@@ -28,4 +28,63 @@ Pr = int(1,x,0,3/4) * int(1,y,1/4,1)
 %% 3-1.2
 clc, clear
 
+syms k x y u v;
 
+% a) find k
+val = int(k*x*int(y,y,0,1),x,0,1); %Double integral
+k = solve( val == 1, k) %Must be equal 1 (p. 122, rule 2)
+
+% b) joint probability distribution function
+% F(x,y)
+
+val = int(k*u*int(v,v,0,y),u,0,x)
+
+
+% c) joint probability of X <= 1/2 and Y > 1/2
+
+Pr_x = int(k*x*int(y,y,0.5,1),0,0.5)
+
+
+
+% d) Marginal function, f_X(x)
+val = int(k*x*y,y,0,1)
+
+%% 3-1.3
+syms x y;
+f_xy = 1;
+
+% a)
+E_XY = int(int(x*y*f_xy,x,0,1),y,0,1)
+
+f_xy = 4*x*y;
+% b)
+E_XY = int(int(x*y*f_xy,x,0,1),y,0,1)
+
+
+%% 3-2.2
+clc, clear
+syms x y;
+f_xy = 4*x*y;
+
+% a) conditional probability density function, f(x|y)
+f_y = int(f_xy,x,0,1);
+f_Xy = f_xy/f_y
+
+
+% b) conditional probability density function, f(y|x)
+f_x = int(f_xy,y,0,1);
+f_Yx = f_xy/f_x
+
+%% 3-2.4
+clc, clear;
+
+% a) generel expression
+syms K x y;
+f_xy = K*exp(-(x^2+y^2+4*x*y));
+
+diff(f_xy,x)
+
+val = solve((2*x+4*y) == 0, x)
+
+% b) If y = 3, estimate X
+X = -2*3

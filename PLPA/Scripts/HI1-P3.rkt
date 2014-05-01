436f46a26495b8cecabdb5abb3e30d2cbc3fa6ba
Rasmus Bækgaard <git@bakgaard.net>
2014-05-01 21:58:32 +0200

    Update PLPA assignment

--- PLPA/Scripts/HI1-P3.rkt
+++ PLPA/Scripts/HI1-P3.rkt
@@ -1,72 +1,56 @@
-;; fold product
-(define foldP
-  (lambda (acc lst)
+;; FP1.scm
+;; Rasmus Bækgaard <10893@iha.dk> 10893
+;;
+
+;fold
+(define foldl
+  (lambda (proc acc lst)
     (if (null? lst)
         acc
-        (foldP 
-               (* acc (car lst))
+        (foldl proc
+               (proc acc (car lst))
                (cdr lst)))))
 
-;; fold1 wrapper
-(define fold1
+;fold product
+(define foldP
   (lambda (lst)
-    (foldP 1 lst)))
+          (foldl * 1 lst)))
 
-;(fold1 '(1 2 3 4 5))
-;(fold1 '(2 3 4))
+;(foldP '(1 2 3))
+;(foldP '(1 2 3 4))
 
-;; fold postive
-(define posPro
-  (lambda (lst x)
-    (if (null? lst) 
-        x 
-        (if (< 0 (car lst))
-            (posPro ;true
-             (cdr lst) (* (car lst) x)) 
-            (posPro ;false
-             (cdr lst) x)))))
-
-; Wrapper
-(define fold2
+(define foldPositive
   (lambda (lst)
-    (posPro lst 1)
-    ))
+    (foldl + 0 lst)))
 
-;(fold2 '(1 2 3 4 5))
-;(fold2 '(2 3 4))
+;(foldPositive '(1 2 3 4 5))
+;(foldPositive '(2 3 4))
 
-; fold3 reimplement length
-(define fold3
-    (lambda (lst)
-      (if (null? lst) 0 
-          (+ 1 (fold3 (cdr lst))))))
-
-;(fold3 (list 1 2 3))
-;(fold3 (list 1 2 3 2))
-
-; fold4 reimplement reverse
-(define foldRev
-  (lambda (lst rev)
-    (if (and (null? lst) (null? rev)) rev
-        (if (null? (cdr lst))
-            (cons (car lst) rev)
-            (foldRev (cdr lst) (cons (car lst) rev))))))
-
-;wrapper
-(define fold4
+(define foldLength
   (lambda (lst)
-    (foldRev lst '() )))
+    (foldl 
+     (lambda (acc d) 
+       (+ acc 1)) 
+     0 lst)))
 
-;(fold4 '())
-;(fold4 '(1 2 3))
+(foldLength '())
+(foldLength (list 1 2 3))
+(foldLength (list 1 2 3 2))
 
-; fold5 reimplement map
-(define fold5
-  (lambda (proc arg)
-    (if (null? arg) '()
-        (cons (proc (car arg))
-              (fold5 proc (cdr arg))))))
+(define foldlReverse
+  (lambda (lst)
+    (foldl 
+     (lambda (acc n) 
+       (append (list n) acc)) 
+     '() lst)))
 
-(fold5 abs (list 1 -2 3))
-(fold5 - (list 1 -2 3))
+;(foldlReverse '(1 2 3 4))
 
+(define foldMap
+  (lambda (proc lst)
+    (foldl 
+     (lambda (acc n) 
+       (append acc (list (proc n)) )) 
+     '() lst)))
+
+;(foldMap abs '( -1 -2 3 4))

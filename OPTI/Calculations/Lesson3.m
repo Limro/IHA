9b3c31540bb4096bf8384ade067d6391765eb1c2
Rasmus Bækgaard <rasmus@bakgaard.net>
2014-09-07 23:17:44 +0200

    Update Lesson 3 for OPTI

--- OPTI/Calculations/Lesson3.m
+++ OPTI/Calculations/Lesson3.m
@@ -1,12 +1,51 @@
 %% Lay 9,4 - 11
 clc, clear
 
-c = [16 10 20]';
+b = [16 10 20]';
 A = [1 1 3 ; 2 1 2]';
-b = [4 5]';
+A = [A eye(3)];
+c = [4 5 0];
+v = [3 4 5];
 
-Arg = [ A eye(3) c; -b' 0 0 0 0]
+Arg = [ A b; -c 0 0 0];
 
+c = [-c 0 0]';
+[tab, res] = simplex(c, A, b, v, 1)
+x1 = 7/4
+x2 = 0
+x3 = 3/4
+M = 43
+
+%% Lay 9,4 - 21
+clc, clear
+
+b = [1 1 1]';
+A = [4 1 -2; %stocks
+    1 3 0; %Bonds
+    -1 0 4]; %Gold
+A = [A] + 3
+A = [A eye(3)];
+c = [1 1 1];
+v = [4 5 6];
+
+Arg = [ A b; -c 0 0 0 0];
+
+c = [-c 0 0 0]';
+simplex(c, A, b, v, 1)
+x1 = 1/13;
+x2 = 9/143;
+x3 = 15/143;
+M = 35/143; %lambda
+y1 = 14/143;
+y2 = 8/143;
+y3 = 1/11;
+
+x_hat = [x1 x2 x3]' * (1/M)
+y_hat = [y1 y2 y3]' * (1/M)
+
+stocks = 35000 * x_hat(1,1)
+bonds = 35000 * x_hat(2,1)
+gold = 35000 * x_hat(3,1)
 %% eksempel
 clc, clear
 
@@ -31,4 +70,4 @@ Arg = [ Arg(1,:) ; Arg(2,:)-1.25*Arg(1,:) ; Arg(3,:)-0.25*Arg(1,:) ; Arg(4,:)+0.
 
 A =  [A eye(3)];
 c = [-c' 0 0 0]';
-simpelx(c, A, b, v, 1)
+simplex(c, A, b, v, 1)

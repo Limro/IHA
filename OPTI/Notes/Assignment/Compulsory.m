6617e228a8c1edf843e3e4c07d22c83939be28bd
Rasmus Bækgaard <rasmus@bakgaard.net>
2014-09-28 21:17:15 +0200

    Huge OPTI delivery update

--- OPTI/Notes/Assignment/Compulsory.m
+++ OPTI/Notes/Assignment/Compulsory.m
@@ -68,4 +68,129 @@ syms x1 x2;
 
 [val1, val2] = solve(Q*[x1;x2]-b == 0)
 
+%%
+clc, clear, close all
+
+syms x;
+f = 1/2*x.^2 -cos(x);
+
+%figure(1);
+%ezplot(f);
+
+fm = diff(f)
+fmm = diff(fm)
+
+fm = @(x) x+sin(x);
+fmm = @(x) cos(x)+1;
+
+x0 = 0.25;
+x1 = x0 - (fm(x0)/fmm(x0))
+x2 = x1 - (fm(x1)/fmm(x1))
+x3 = x2 - (fm(x2)/fmm(x2))
+%%
+clc ,clear
+syms xx1 xx2 a;
+x = [xx1;xx2];
+
+Q = [2 -2;-2 6];
+b = [0;3];
+f(x) = 1/2*x.' * Q * x - x.' * b;
+g(x) = Q*x-b;
+x0 = [1;1];
+
+arg = x0 - a*g(x0(1),x0(2));
+Phi0(a) = f(arg(1), arg(2));
+
+a0 = solve(diff(Phi0(a)) == 0);
+x1 =  x0-a0*g(x0(1),x0(2));
+
+arg = x1 - a*g(x1(1),x1(2));
+Phi1(a) = f(arg(1), arg(2))
+
+a1 = solve(diff(Phi1(a)) == 0)
+x2 =  x1-a1*g(x1(1),x1(2))
+
+arg = x2 - a*g(x2(1),x2(2));
+Phi2(a) = f(arg(1), arg(2))
+
+a2 = solve(diff(Phi2(a)) == 0)
+x3 =  x2-a2*g(x2(1),x2(2))
+
+ezsurf(f(xx1,xx2))
+
+lamMax = max(eigs(Q))
+
+%%
+clc, clear
+syms yi A ti s;
+r = yi - A*exp(-(ti^2/s^2));
+J1 = diff(r, A)
+J2 = diff(r, s)
+%%
+
+%Newton Gauss data fitting
+%HKa sep. 2013
+clear close all  clc;
+%simulate dataset
+t=(-2:.01:2);
+x_gold=[1;.1];
+y=x_gold(1)*exp(-(t).^2/x_gold(2)^2);
+y=y+.03*randn(size(y));
+figure(1)
+plot(y,'.b'); 
+hold on;
+%pause
+xx = [];
+x = x_gold+[.9;.9];
+for k = 1:75
+    %Jacobian
+    J = [ 
+        -exp(-t.^2/x(2)^2) ; 
+        -(2*x(1)*t.^2.*exp(-t.^2/x(2)^2))/x(2)^3 ].';
+    
+    % The function
+    f = x(1)*exp(-(t).^2/x(2)^2);
+    
+    % r
+    r = y - f;
+    x = x - (J.'*J)^(-1) * (J.' * r.');
+    xx = [xx,x];
+    plot(f)  
+end
+figure(2)
+plot(xx(1,:),'.r');hold on;
+plot(xx(2,:),'.g');
+xlabel('Iterations')
+
+
+
+%%
+clc ,clear
+syms xx1 xx2 a;
+x = [xx1;xx2];
+
+Q = [2 -2;-2 6];
+b = [0;3];
+f(x) = 1/2*x.' * Q * x - x.' * b;
+g(x) = Q*x-b;
+x0 = [1;1];
+
+gr(x) = Q*x-b;
+
+g0 = gr(x0(1), x0(2))
+d0 = -g0;
+a0 = -(g0.'*d0)/(d0.'*Q*d0)
+x1 = x0+a0*d0
+
+g1 = gr(x1(1), x1(2))
+b0 = g1.'*Q*d0/(d0'*Q*d0)
+d1 = -g1+b0*d0
+a1 = -(g1.'*d1)/(d1.'*Q*d1)
+x2 = x1+a1*d1
+% d1 = -g1;
+% a1 = -(g1.'*d1)/(d1.'*Q*d1)
+% x2 = x1+a1*d1
+
+
+
 

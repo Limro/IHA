cdd4f7a582eccfc1d74f0e2abffc54d9bfbe35bf
Rasmus Bækgaard <git@bakgaard.net>
2014-11-17 23:17:56 +0100

    Add project for ADPE and rearange Material

---
+++ ADPE/Exercises/Context/Project/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Speech.Synthesis;
+using Project;
+
+namespace SampleSynthesis
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			var reader = new Reader();
+			var timer = new Timerss();
+	
+			Messages.
+
+			var res = timer.SleepAsync(2000).Result();
+
+			Console.WriteLine(res);
+
+			reader.Read("Hello");
+			reader.Read("Remember: a train says toot");
+
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
+		}
+	}
+
+	public List<string> Messages = new List<string>();
+
+	class Reader
+	{
+		SpeechSynthesizer syn;
+
+		public Reader()
+		{
+			syn = new SpeechSynthesizer();
+			syn.SetOutputToDefaultAudioDevice();
+		}
+
+		public void Read(string message)
+		{
+			syn.Speak(message);
+		}
+
+
+
+
+	}
+
+}
---
+++ ADPE/Exercises/Context/Project/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Project
+{
+	class Timerss
+	{
+
+		public async Task<string> SleepAsync(int millisecondsTimeOut)
+		{
+			TaskCompletionSource<bool> tcs = null;
+			var t = new Timer(delegate { tcs.TrySetResult(true);}, null, -1, -1);
+			tcs = new TaskCompletionSource<bool>(t);
+			t.Change(millisecondsTimeOut, -1);
+			await Console.WriteLine("Hejsan");
+			return "Hejsan";
+			//return tcs.Task;
+		}
+	}
+	
+
+	class StateObjClass
+	{
+		// Used to hold parameters for calls to TimerTask. 
+		public int SomeValue;
+		public System.Threading.Timer TimerReference;
+		public bool TimerCanceled;
+	}
+}
